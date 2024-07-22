99c1458ff00d6d5e1e7b084a411d0e6e38d27d63
krishmint <162588734+krishmint@users.noreply.github.com>
2024-07-22 21:51:49 +0530

    Update s3-notification-triggers.sh

--- aws-event-triggering/s3-notification-triggers.sh
+++ aws-event-triggering/s3-notification-triggers.sh
@@ -3,20 +3,17 @@
 set -x
 
 # Store the AWS account ID in a variable
-aws_account_id=$(aws sts get-caller-identity --query 'Account' --output text)
-
-# Print the AWS account ID from the variable
-echo "AWS Account ID: $aws_account_id"
+aws_accId=$(aws sts get-caller-identity| jq -r '.Account')
 
 # Set AWS region and bucket name
 aws_region="us-east-1"
-bucket_name="abhishek-ultimate-bucket"
+bucket_name="eventtriggerbucket"
 lambda_func_name="s3-lambda-function"
 role_name="s3-lambda-sns"
-email_address="zyz@gmail.com"
+email_address="krisnendu007@gmail.com"
 
 # Create IAM Role for the project
-role_response=$(aws iam create-role --role-name s3-lambda-sns --assume-role-policy-document '{
+role-arn=$(aws iam create-role --role-name $role_name --assume-role-policy-document '{
   "Version": "2012-10-17",
   "Statement": [{
     "Action": "sts:AssumeRole",
@@ -29,13 +26,8 @@ role_response=$(aws iam create-role --role-name s3-lambda-sns --assume-role-poli
       ]
     }
   }]
-}')
+}'| jq -r '.Role.Arn')
 
-# Extract the role ARN from the JSON response and store it in a variable
-role_arn=$(echo "$role_response" | jq -r '.Role.Arn')
-
-# Print the role ARN
-echo "Role ARN: $role_arn"
 
 # Attach Permissions to the Role
 aws iam attach-role-policy --role-name $role_name --policy-arn arn:aws:iam::aws:policy/AWSLambda_FullAccess
@@ -46,9 +38,6 @@ bucket_output=$(aws s3api create-bucket --bucket "$bucket_name" --region "$aws_r
 
 # Print the output from the variable
 echo "Bucket creation output: $bucket_output"
-
-# Upload a file to the bucket
-aws s3 cp ./example_file.txt s3://"$bucket_name"/example_file.txt
 
 # Create a Zip file to upload Lambda Function
 zip -r s3-lambda-function.zip ./s3-lambda-function
@@ -62,7 +51,7 @@ aws lambda create-function \
   --handler "s3-lambda-function/s3-lambda-function.lambda_handler" \
   --memory-size 128 \
   --timeout 30 \
-  --role "arn:aws:iam::$aws_account_id:role/$role_name" \
+  --role "$role_arn" \
   --zip-file "fileb://./s3-lambda-function.zip"
 
 # Add Permissions to S3 Bucket to invoke Lambda
